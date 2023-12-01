bfa063a011e4beeaf08e5d94fb504c3edfa1321f
Huilensolis <xywvanz@gmail.com>
2023-12-01 18:25:36 -0300

    change priamry color and make auth layout responsive

--- src/app/(site)/(guest)/auth/layout.tsx
+++ src/app/(site)/(guest)/auth/layout.tsx
@@ -3,14 +3,14 @@ import { Header } from "../components/header/header";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <main className="flex w-full h-screen">
+    <main className="flex w-full h-screen lg:px-0 px-2">
       <section className="w-full h-full flex flex-col items-center bg-neutral-100 dark:bg-neutral-900">
         <Header />
         <div className="h-full w-full flex flex-col justify-center items-center">
           {children}
         </div>
       </section>
-      <section className="h-full w-full bg-blue-300"></section>
+      <section className="h-full w-full bg-emerald-400 lg:inline-block hidden"></section>
     </main>
   );
 }
