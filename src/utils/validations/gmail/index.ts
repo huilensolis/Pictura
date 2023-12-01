89f3eb9e2deb537894f053d17b189cd7ae6b32b2
Huilensolis <xywvanz@gmail.com>
2023-12-01 17:29:47 -0300

    delete console.log

--- src/utils/validations/gmail/index.ts
+++ src/utils/validations/gmail/index.ts
@@ -3,7 +3,6 @@ import { EmailSchema } from "./gmail.models";
 export function validateEmail(email: string): boolean {
   try {
     EmailSchema.parse(email);
-    console.log("correct email format");
     return true;
   } catch (error) {
     console.log(error);
