9e58240795288ab9d65f67747f4a1a4025fa71b5
Huilensolis <xywvanz@gmail.com>
2023-11-30 21:56:59 -0300

    improve input padding

--- src/components/ui/input/index.tsx
+++ src/components/ui/input/index.tsx
@@ -39,7 +39,7 @@ export function Input({
           error
             ? "bg-red-400"
             : "bg-neutral-100 focus:bg-neutral-200 dark:bg-neutral-800"
-        } rounded-xl focus:text-neutral-900 text-neutral-600 p-3 dark:focus:text-neutral-50 dark:text-neutral-400 dark:placeholder:text-neutral-500`}
+        } rounded-xl focus:text-neutral-900 text-neutral-600 py-2 px-3 dark:focus:text-neutral-50 dark:text-neutral-400 dark:placeholder:text-neutral-500`}
       />
       {error && <span>{error}</span>}
     </div>
