7a064277c4c73749d52b6339e1eba267ccbc8030
Huilensolis <xywvanz@gmail.com>
2023-12-04 18:06:13 -0300

    feature: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         className={`${inter.className} min-h-screen w-full flex flex-col items-center`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
