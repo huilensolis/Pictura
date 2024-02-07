1d07e8313a670c4ccf0391fd794a41a2ea44d1e6
Huilensolis <xywvanz@gmail.com>
2024-02-07 20:06:06 -0300

    fix(app): 83 fix broken link to accesibility settings page

--- src/app/(site)/app/settings/page.tsx
+++ src/app/(site)/app/settings/page.tsx
@@ -1,28 +1,28 @@
-'use client';
+"use client";
 
 import {
   ChevronLeftIcon,
   CircleUserIcon,
   LockIcon,
   SunMoonIcon,
-} from 'lucide-react';
-import Link from 'next/link';
-import { ILink } from '../models/nav-links/nav-links.models';
-import { MobileAside } from '@/components/feature/aside-menu/mobile-aside';
+} from "lucide-react";
+import Link from "next/link";
+import { ILink } from "../models/nav-links/nav-links.models";
+import { MobileAside } from "@/components/feature/aside-menu/mobile-aside";
 
 export default function SettingsPage() {
   return (
-    <div className='w-full flex justify-center items-start bg-neutral-200 dark:bg-cm-gray h-screen'>
-      <div className='w-full max-w-2xl flex flex-col items-center justify-center'>
-        <div className='w-full flex px-5 pt-10 pb-1'>
+    <div className="w-full flex justify-center items-start bg-neutral-200 dark:bg-cm-gray h-screen">
+      <div className="w-full max-w-2xl flex flex-col items-center justify-center">
+        <div className="w-full flex px-5 pt-10 pb-1">
           <Link
-            href='/app'
-            className='w-max flex justify-center items-center p-2 bg-neutral-300 dark:bg-neutral-700 rounded-xl dark:text-neutral-200 text-neutral-900'
+            href="/app"
+            className="w-max flex justify-center items-center p-2 bg-neutral-300 dark:bg-neutral-700 rounded-xl dark:text-neutral-200 text-neutral-900"
           >
             <ChevronLeftIcon />
           </Link>
         </div>
-        <div className='w-full'>
+        <div className="w-full">
           <SettingsMenu />;
         </div>
       </div>
@@ -34,19 +34,19 @@ interface ITLink extends ILink {
 }
 const LINKS: ITLink[] = [
   {
-    title: 'Edit Profile',
+    title: "Edit Profile",
     icon: CircleUserIcon,
-    href: 'settings/section/profile',
+    href: "settings/section/profile",
   },
   {
-    title: 'Reset Password',
+    title: "Reset Password",
     icon: LockIcon,
-    href: 'settings/section/reset-password',
+    href: "settings/section/reset-password",
   },
   {
-    title: 'Accessibility',
+    title: "Accessibility",
     icon: SunMoonIcon,
-    href: 'settings/section/Accessibility',
+    href: "settings/section/accessibility",
   },
 ];
 
@@ -54,8 +54,8 @@ function SettingsMenu() {
   return (
     <MobileAside
       header={{
-        title: 'Account Settings',
-        subtitle: 'Configurate your account',
+        title: "Account Settings",
+        subtitle: "Configurate your account",
       }}
       showBorderOnLinks
       links={LINKS}
