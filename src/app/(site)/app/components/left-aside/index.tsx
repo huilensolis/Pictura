629c9b42b8fee201d93a987a6633827578a57aa3
Huilensolis <xywvanz@gmail.com>
2024-03-27 18:17:53 -0300

    refactor(client): 136 improve new users ui

--- src/app/(site)/app/components/left-aside/index.tsx
+++ src/app/(site)/app/components/left-aside/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { LinkIcon } from "lucide-react";
+import { ImageOff, LinkIcon } from "lucide-react";
 import { useUserProfile } from "@/hooks/use-user-profile";
 import { Database } from "@/supabase/types";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -53,20 +53,26 @@ function UserProfile() {
 
     return (
       <article className="w-full flex items-center gap-4 hover:bg-neutral-300 dark:hover:bg-cm-lighter-gray transition-all delay-75 py-2 px-4 rounded-full">
-        <LazyImage
-          alt={`${userProfile?.name ?? ""}'s Profile Picture`}
-          className="w-12 h-12 rounded-full object-cover object-center aspect-square"
-          skeletonClassName="w-12 h-12 rounded-full animate-pulse"
-          src={userProfile?.avatar_url ?? ""}
-          width={48}
-          height={48}
-        />
+        {userProfile.avatar_url ? (
+          <LazyImage
+            alt={`${userProfile.name}'s Profile Picture`}
+            className="w-12 h-12 rounded-full object-cover object-center aspect-square"
+            skeletonClassName="w-12 h-12 rounded-full animate-pulse"
+            src={userProfile.avatar_url}
+            width={48}
+            height={48}
+          />
+        ) : (
+          <div className="flex h-12 w-12 items-center justify-center bg-neutral-300 rounded-full">
+            <ImageOff className="text-neutral-400 2-5 h-5" />
+          </div>
+        )}
         <div>
           <span className="text-neutral-800 dark:text-neutral-50 font-semibold text-lg">
-            {getShortName(userProfile.name ?? "no name yet", 12)}
+            {getShortName(userProfile.name ?? "no name yet", 9)}
           </span>
           <p className="text-neutral-600 dark:text-gray-300">
-            @{getShortName(userProfile.username ?? "no username yet", 12)}
+            @{getShortName(userProfile.username ?? "no username yet", 9)}
           </p>
         </div>
         <LinkIcon className="ml-5 text-neutral-900 dark:text-neutral-50" />
