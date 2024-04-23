d882051a3de3bbfd013ceb2616e096f40dd36942
Huilensolis <xywvanz@gmail.com>
2024-04-23 18:29:29 -0300

    fix(app): 125 edit profile form

--- src/app/(site)/app/settings/section/profile/components/form-skeleton/form-skeleton.component.tsx
+++ src/app/(site)/app/settings/section/profile/components/form-skeleton/form-skeleton.component.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 export function FormSkeleton() {
   return (
     <div className="flex flex-col gap-2 w-full">
-      <header className="relative w-full mb-10">
+      <header className="relative w-full mb-16">
         <Skeleton className="w-full h-56 rounded-lg" />
-        <Skeleton className="w-32 h-32 rounded-full absolute left-5 -bottom-10" />
+        <Skeleton className="w-32 h-32 rounded-full absolute top-40 left-5" />
       </header>
       <div className="flex flex-col gap-1">
         <Skeleton className="w-28 h-3 rounded-lg" />
