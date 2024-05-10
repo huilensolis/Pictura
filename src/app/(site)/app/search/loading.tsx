24a2486acdc03def874d2840a47e7c38cae37aff
Huilensolis <xywvanz@gmail.com>
2024-05-10 13:29:31 -0300

    feat: add skeleton to search page

--- src/app/(site)/app/search/loading.tsx
+++ src/app/(site)/app/search/loading.tsx
@@ -1,3 +1,17 @@
+import { PostsGridContainer } from "@/components/feature/posts-grid/components/posts-grid-container";
+import { PostsGridSkeleton } from "@/components/feature/posts-grid/components/posts-grid-skeleton";
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
-  return <p>loading</p>;
+  return (
+    <>
+      <div className="w-full flex flex-col gap-2">
+        <Skeleton className="w-24 h-6 rounded-sm" />
+        <Skeleton className="w-full h-10" />
+      </div>
+      <PostsGridContainer>
+        <PostsGridSkeleton />
+      </PostsGridContainer>
+    </>
+  );
 }
