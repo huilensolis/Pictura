bb3b58d19c65fd2b6ac998323a4ef29878764fe0
Huilensolis <xywvanz@gmail.com>
2024-05-17 14:09:08 -0300

    fix: posts grid row image skeleton size

--- src/components/feature/posts-grid/components/posts-grid-row/posts-grid-row.component.tsx
+++ src/components/feature/posts-grid/components/posts-grid-row/posts-grid-row.component.tsx
@@ -5,6 +5,7 @@ import { ClientRouting } from "@/models/routing/client";
 import { Database } from "@/supabase/types";
 import { LazyImage } from "@/components/feature/lazy-image";
 import { PostRowFooter } from "../post-row-footer";
+import { useCallback, useState } from "react";
 
 export function PostsGridRow({
   post,
@@ -15,8 +16,16 @@ export function PostsGridRow({
   collection?: Database["public"]["Tables"]["collection"]["Row"];
   userId: Database["public"]["Tables"]["users"]["Row"]["id"];
 }) {
+  const [columnWidth, setColumnWidth] = useState(0);
+
+  const column = useCallback((node: HTMLLIElement) => {
+    if (node) {
+      setColumnWidth(node.offsetWidth);
+    }
+  }, []);
+
   return (
-    <li className="w-full mb-6 rounded-md flex flex-col gap-1">
+    <li className="w-full mb-6 rounded-md flex flex-col gap-1" ref={column}>
       <Link
         href={ClientRouting.post().page(post.id)}
         className="w-full rounded-md"
@@ -24,6 +33,8 @@ export function PostsGridRow({
         <LazyImage
           src={post.asset_url}
           alt={post.title}
+          width={columnWidth}
+          height={(post.asset_height / post.asset_width) * columnWidth}
           className="flex w-full h-full rounded-md object-cover object-center"
           skeletonClassName="w-full h-full rounded-md"
           skeletonBgColor={post.asset_color || undefined}
